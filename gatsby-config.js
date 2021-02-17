93f5ff615f3d0553212451a36f10ada99a387abc
sasamori.t <takumi.sasamori.works@gmail.com>
2021-02-17 11:25:08 +0900

    [modify]ティザーテストアップ

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,60 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `@lekoarts/gatsby-theme-jodie`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          { name: `Projects`, slug: `/projects` },
+          { name: `About`, slug: `/about` },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `2315642426`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
-        start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        name: `jodie - @lekoarts/gatsby-theme-jodie`,
+        short_name: `jodie`,
+        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#b75e09`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
-};
+}
